a145fd2aca005548fdd875f989d8ea89c442ba1e
jnguegan <jnguegan@gmail.com>
2026-04-20 20:34:56 +0100

    Fix formatting issue in supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,6 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const SUPABASE_URL = 'https://xkbxwdtioduklzlrhxhs.supabase.co';
+
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrYnh3ZHRpb2R1a2x6bHJoeGhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNjk2NzEsImV4cCI6MjA5MTc0NTY3MX0.RfmbnIEm7Oa52ZskPGedUQiOAQHC8P3LhX2P4C8ZPkY';
 
 window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
