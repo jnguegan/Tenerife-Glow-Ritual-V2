0474876e1a2ad06bfc782fb05b959f568023eac8
jnguegan <jnguegan@gmail.com>
2026-04-20 16:26:15 +0100

    Create supabase-client.js

---
+++ js/supabase-client.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://xkbxwdtioduklzlrhxhs.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrYnh3ZHRpb2R1a2x6bHJoeGhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNjk2NzEsImV4cCI6MjA5MTc0NTY3MX0.RfmbnIEm7Oa52ZskPGedUQiOAQHC8P3LhX2P4C8ZPkY';
+
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
